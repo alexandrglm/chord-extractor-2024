22af30a299fecad71ae058e1139fc54e07453b08
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-09-15 22:17:05 +0200

    Update db.py

--- db.py
+++ db.py
@@ -16,6 +16,8 @@ def generate_db_html():
     <!DOCTYPE html>
     <html>
     <head>
+        <link rel="icon" href="./favicon.ico" sizes="any">
+        <link rel="icon" href="./favicon.svg" type="image/svg+xml">
         <meta charset="UTF-8">
         <title>MUSIC DATABASE</title>
         <script src="./engine/scripts/chart.js"></script>
