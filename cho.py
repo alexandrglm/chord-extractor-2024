05767fdee78bbc8d911fc160dea6e62f5c6f5711
Alexandr Gomez <148530039+alexandrglm@users.noreply.github.com>
2024-09-15 16:05:09 +0200

    Update cho.py

--- cho.py
+++ cho.py
@@ -16,10 +16,6 @@ def sanitize_filename(input_str):
     sanitized = re.sub(r'[^\w\s-]', '', normalized)
     return sanitized.strip().replace(' ', '_')
 
-def sanitize_url(url):
-    from urllib.parse import quote
-    return quote(url, safe='/:')
-
 def extract_chords_from_audio(audio_file):
     chordino = Chordino(roll_on=1.0)
     chords = chordino.extract(audio_file)
@@ -30,20 +26,18 @@ def get_bpm(audio_file):
     onset_env = librosa.onset.onset_strength(y=y, sr=sr)
     tempo, beat_frames = librosa.beat.beat_track(onset_envelope=onset_env, sr=sr)
     beat_times = librosa.frames_to_time(beat_frames, sr=sr)
-    return tempo[0] if isinstance(tempo, np.ndarray) and tempo.size == 1 else tempo, beat_times
+    if isinstance(tempo, np.ndarray):
+        tempo = tempo[0] if tempo.size == 1 else tempo
+    return tempo, beat_times
 
-def get_reference_pitch_segment(audio_file, start_time, end_time):
-    y, sr = librosa.load(audio_file, offset=start_time, duration=end_time - start_time)
-    pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
+def get_reference_pitch_segment(y, sr, start_time, end_time):
+    segment = y[int(sr * start_time): int(sr * end_time)]
+    pitches, magnitudes = librosa.piptrack(y=segment, sr=sr)
     magnitude_median = np.median(magnitudes)
     a4_range = (440.0 * np.power(2, -0.5), 440.0 * np.power(2, 0.5))
     a4_frequencies = [pitches[i, j] for i in range(pitches.shape[0]) for j in range(pitches.shape[1])
                       if magnitudes[i, j] > magnitude_median and a4_range[0] <= pitches[i, j] <= a4_range[1]]
-    if a4_frequencies:
-        a4_frequency = np.mean(a4_frequencies)
-    else:
-        a4_frequency = 440.0
-    return a4_frequency
+    return np.mean(a4_frequencies) if a4_frequencies else 440.0
 
 def get_tone_from_frequencies(frequencies, reference_freq):
     if len(frequencies) == 0:
@@ -53,13 +47,14 @@ def get_tone_from_frequencies(frequencies, reference_freq):
     return librosa.midi_to_note(median_tone)
 
 def get_tone_at_beats(audio_file, beat_times):
+    y, sr = librosa.load(audio_file)
     tones_at_beats = []
     for i, beat_time in enumerate(beat_times[:-1]):
         start_time = beat_time
         end_time = beat_times[i + 1]
-        reference_freq = get_reference_pitch_segment(audio_file, start_time, end_time)
-        y, sr = librosa.load(audio_file, offset=start_time, duration=end_time - start_time)
-        pitches, magnitudes = librosa.piptrack(y=y, sr=sr)
+        reference_freq = get_reference_pitch_segment(y, sr, start_time, end_time)
+        segment = y[int(sr * start_time): int(sr * end_time)]
+        pitches, magnitudes = librosa.piptrack(y=segment, sr=sr)
         magnitude_median = np.median(magnitudes)
         significant_pitches = pitches[magnitudes > magnitude_median]
         if significant_pitches.size > 0:
@@ -80,20 +75,37 @@ def convert_ndarray_to_list(obj):
 def load_scales():
     return scales
 
-def match_chords_to_scales(chords, scales):
-    chord_names = set(chord for chord in chords)
+def simplify_chord(chord_name):
+    """Simplifica el nombre del acorde para asegurar una comparación consistente."""
+    # Ejemplo de simplificación: quitar posibles alteraciones como 'maj7', 'm7', etc.
+    if '/' in chord_name:
+        chord_name = chord_name.split('/')[0]
+    return chord_name
+
+def match_chords_to_scales(chords, scales, tolerance=0.7):
+    chord_names = set(simplify_chord(chord.chord) for chord in chords)
+    print(f"Simplified Chord names: {chord_names}")  # Debugging line
+
     best_match = None
     best_match_score = 0
-    for key, scale_types in scales.items():
+    for keynote, scale_types in scales.items():
         for scale_name, scale_notes in scale_types.items():
             scale_notes_set = set(scale_notes)
-            match_score = len(chord_names.intersection(scale_notes_set))
-            if match_score > best_match_score:
-                best_match = (key, scale_name)
+            print(f"Checking scale: Keynote: {keynote}, Scale: {scale_name}, Notes: {scale_notes_set}")  # Debugging line
+
+            intersection_count = len(chord_names.intersection(scale_notes_set))
+            match_score = intersection_count / len(chord_names) if chord_names else 0
+            print(f"Match score for Keynote {keynote}, Scale {scale_name}: {match_score}")  # Debugging line
+
+            if match_score >= tolerance and match_score > best_match_score:
+                best_match = keynote
                 best_match_score = match_score
+
+    print(f"Best match: {best_match}, Best match score: {best_match_score}")  # Debugging line
     return best_match
 
-def update_chords_db(artist, title, chords, bpm, tempo_changes, tones_at_beats, keynote):
+
+def update_chords_db(artist, title, chords, bpm, tempo_changes, tones_at_beats, matches):
     if os.path.exists(DB_FILE):
         try:
             with open(DB_FILE, "r", encoding="utf-8") as f:
@@ -106,17 +118,11 @@ def update_chords_db(artist, title, chords, bpm, tempo_changes, tones_at_beats, 
 
     chord_counts = {}
     for chord in chords:
-        chord_name = chord.chord
-        if chord_name not in chord_counts:
-            chord_counts[chord_name] = 1
-        else:
-            chord_counts[chord_name] += 1
+        chord_name = str(chord)
+        chord_counts[chord_name] = chord_counts.get(chord_name, 0) + 1
 
     tempo_changes = convert_ndarray_to_list(tempo_changes)
     tones_at_beats_serializable = convert_ndarray_to_list(tones_at_beats)
-
-    scales = load_scales()
-    best_match = match_chords_to_scales(chord_counts.keys(), scales)
 
     song_entry = {
         "artist": artist,
@@ -125,7 +131,7 @@ def update_chords_db(artist, title, chords, bpm, tempo_changes, tones_at_beats, 
         "bpm": bpm,
         "tempo_changes": tempo_changes,
         "tones_at_beats": tones_at_beats_serializable,
-        "keynote": best_match[0] if best_match else "Unknown"
+        "matches": matches
     }
 
     chords_db.append(song_entry)
@@ -134,17 +140,6 @@ def update_chords_db(artist, title, chords, bpm, tempo_changes, tones_at_beats, 
         json.dump(chords_db, f, indent=4, ensure_ascii=False)
 
     return song_entry
-
-def determine_key_from_chords(chords):
-    chord_names = [chord.chord for chord in chords]
-
-    scales = load_scales()
-    for keynote, scale_variations in scales.items():
-        for scale_name, scale_chords in scale_variations.items():
-            if all(chord in scale_chords for chord in chord_names):
-                return keynote
-
-    return "Unknown"
 
 def main():
     audio_file = input("Enter the path to the audio file: ")
@@ -167,7 +162,7 @@ def main():
 
     chords = extract_chords_from_audio(audio_file)
 
-    keynote = determine_key_from_chords(chords)
+    keynote = match_chords_to_scales(chords, load_scales())
     print(f"Keynote: {keynote}")
 
     song_entry = update_chords_db(artist_name, song_title, chords, tempo, beat_times, tones_at_beats, keynote)
